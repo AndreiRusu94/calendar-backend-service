172e9b147f70145c7c7badc55547f777f4c080f9
Andrei Rusu <andrei.rusu2094@gmail.com>
2019-12-28 17:45:45 +0200

    Setup project structure

---
+++ src/main/java/calendar/backend/service/calendarbackendservice/CalendarBackendServiceApplication.java
@@ -0,0 +1,13 @@
+package calendar.backend.service.calendarbackendservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CalendarBackendServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CalendarBackendServiceApplication.class, args);
+	}
+
+}
