d103bac75fe82642069572a1ac3168f506b735ec
Andrei Rusu <andrei.rusu2094@gmail.com>
2024-12-15 10:59:23 +0200

    Opened the app module to enable reflection and added missing modules

--- app/src/main/java/module-info.java
+++ app/src/main/java/module-info.java
@@ -1,6 +1,8 @@
-module app {
+open module app {
     requires spring.boot;
     requires spring.boot.autoconfigure;
     requires service;
+    requires spring.core;
+    requires spring.beans;
     requires data;
 }
